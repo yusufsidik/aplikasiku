2aa787a44ed36fa23b4000121c9fdffc08135f70
yusufsidik <yusufsidik36@gmail.com>
2023-03-20 21:07:23 +0800

    Learning event listener and useState

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -4,16 +4,20 @@ import Card from "./Card";
 import { useState } from "react";
 export default function ExpenseItem({title, price, date}) {
   const [count, setCount] = useState(1);
+  const [judul, setJudul] = useState(title);
   const click = () => {
     setCount(count+1);
   }
+
+  const changeJudul = () => setJudul(`${judul} - ini dirubah`);
   return (
     <Card className="expense-item">
       <p>{count}</p>
       <button onClick={() => click()}>tambah</button>
+      <button onClick={changeJudul}>Ubah Title</button>
       <ExpenseDate tanggal={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{judul}</h2>
         <div className="expense-item__price">Rp.{price}</div>
       </div>
     </Card>
